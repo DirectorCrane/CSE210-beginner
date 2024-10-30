da15a46869e341681df0a95263e773805eadd51f
DirectorCrane <ethanizaaks19@gmail.com>
2024-10-30 02:51:09 +0200

    Add files via upload
    Done with Week 6 Learning

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        
+
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        Console.WriteLine ($"Square Color: {square.GetColor()}, Area: {square.GetArea()}");
+        Console.WriteLine ($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+        Console.WriteLine ($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape> {square, rectangle, circle};
+        
+        Console.WriteLine("\nShape Details from List:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
+
+
+
     }
 }
